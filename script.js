94e5962acd8c39db84db0878b5663f97c056f01f
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-06-16 07:57:48 +1000

    Create function to create the board and fill it with cells

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Hello")
+function Players(name, symbol) {
+    return {
+        name,
+        symbol
+    }
+}
+
+function Gameboard() {
+    let gridSize = 3;
+    let board = [];
+
+    const Cell = function () {
+        let player = "";
+
+        return {
+            player,
+        }
+    }
+
+    // Create the gameboard
+    for (i = 0; i < gridSize; i++) {
+        board[i] = [];
+        for (j = 0; j < gridSize; j++) {
+            board[i].push(new Cell());
+        }
+    }
+    
+
+    return {
+        board
+    }
+}
+
+function GameController() {
+
+    const board = Gameboard();
+
+    console.log(board);
+
+}
+
+GameController();
